1de28ee39131ed83f6ee39ee60b9d3655ec2e18b
procus <demon17@o2.pl>
2015-05-27 17:43:24 +0000

    dodanie komentarzy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  
-  
   root 'welcome#index'
+  
+  resources :articles do
+    resources :comments
+  end
   
   resources :articles
   
